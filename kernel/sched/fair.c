4247d3e4b784b577e31e998fb1423902e5f0e267
varunhardgamer <gamelovr695@gmail.com>
2018-04-30 11:13:13 +0000

    Revert "sched: Add controls for sched features"
    This reverts commit eb5a5982e412e4c11b290f304bda480050f2f523.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4762,19 +4762,6 @@ static void check_spread(struct cfs_rq *cfs_rq, struct sched_entity *se)
 #endif
 }
 
-static unsigned int Lgentle_fair_sleepers = 0;
-static unsigned int Larch_power = 0;
-
-void relay_gfs(unsigned int gfs)
-{
-	Lgentle_fair_sleepers = gfs;
-}
-
-void relay_ap(unsigned int ap)
-{
-	Larch_power = ap;
-}
-
 static void
 place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 {
@@ -4797,7 +4784,7 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 		 * Halve their sleep time's effect, to allow
 		 * for a gentler effect of sleepers:
 		 */
-		if (Lgentle_fair_sleepers)
+		if (sched_feat(GENTLE_FAIR_SLEEPERS))
 			thresh >>= 1;
 
 		vruntime -= thresh;
@@ -4812,12 +4799,10 @@ static void check_enqueue_throttle(struct cfs_rq *cfs_rq);
 static void
 enqueue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 {
- 	/*
-  	 * If we're the current task, we must renormalise before calling
-  	 * update_curr().
+	/*
 	 * Update the normalized vruntime before updating min_vruntime
 	 * through calling update_curr().
-  	 */
+	 */
 	if (!(flags & ENQUEUE_WAKEUP) || (flags & ENQUEUE_WAKING))
 		se->vruntime += cfs_rq->min_vruntime;
 
@@ -7975,30 +7960,17 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	unsigned long capacity = SCHED_CAPACITY_SCALE;
 	struct sched_group *sdg = sd->groups;
 
-<<<<<<< HEAD
 	if (sched_feat(ARCH_CAPACITY))
 		capacity *= arch_scale_cpu_capacity(sd, cpu);
 	else
 		capacity *= default_scale_cpu_capacity(sd, cpu);
-=======
-	if ((sd->flags & SD_SHARE_CPUPOWER) && weight > 1) {
-		if (Larch_power)
-			power *= arch_scale_smt_power(sd, cpu);
-		else
-			power *= default_scale_smt_power(sd, cpu);
->>>>>>> db5b037... sched: Add controls for sched features
 
 	capacity >>= SCHED_CAPACITY_SHIFT;
 
 	sdg->sgc->capacity_orig = capacity;
 
-<<<<<<< HEAD
 	if (sched_feat(ARCH_CAPACITY))
 		capacity *= arch_scale_freq_capacity(sd, cpu);
-=======
-	if (Larch_power)
-		power *= arch_scale_freq_power(sd, cpu);
->>>>>>> db5b037... sched: Add controls for sched features
 	else
 		capacity *= default_scale_capacity(sd, cpu);
 
