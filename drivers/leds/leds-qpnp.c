dbd90634d3eb0f469c75e23c6d25ba477e15aa8d
varunhardgamer <gamelovr695@gmail.com>
2018-05-19 08:54:27 +0000

    Revert "leds-qpnp: Notification LED control - V1.1a"
    This reverts commit 29c41b08578dc3c98cc1459bebf4b71565d515e5.

--- drivers/leds/leds-qpnp.c
+++ drivers/leds/leds-qpnp.c
@@ -2362,8 +2362,7 @@ static ssize_t pause_lo_store(struct device *dev,
 		return ret;
 	led = container_of(led_cdev, struct qpnp_led_data, cdev);
 
-	if (check_for_notification_led(led_cdev))
-		pause_lo = convert_pause_lo_store(pause_lo);
+	pause_lo = convert_pause_lo_store(pause_lo);
 
 	switch (led->id) {
 	case QPNP_ID_LED_MPP:
