789d9a131c48659703244d5b21188087cd5fcc43
varunhardgamer <gamelovr695@gmail.com>
2018-06-02 08:07:05 +0000

    Revert "do d_instantiate/unlock_new_inode combinations safely"
    This reverts commit 51fb0c2db9f77393e76c410909673c60b48fd4d4.

--- fs/f2fs/namei.c
+++ fs/f2fs/namei.c
@@ -177,7 +177,7 @@ static int f2fs_create(struct inode *dir, struct dentry *dentry, umode_t mode,
 
 	alloc_nid_done(sbi, ino);
 
-	d_instantiate_new(dentry, inode);
+	d_instantiate(dentry, inode);
 	unlock_new_inode(inode);
 
 	if (IS_DIRSYNC(dir))
@@ -466,7 +466,6 @@ static int f2fs_symlink(struct inode *dir, struct dentry *dentry,
 	f2fs_unlock_op(sbi);
 	alloc_nid_done(sbi, inode->i_ino);
 
-<<<<<<< HEAD
 	if (f2fs_encrypted_inode(inode)) {
 		struct qstr istr = QSTR_INIT(symname, len);
 		struct fscrypt_str ostr;
@@ -524,9 +523,6 @@ err_out:
 	kfree(sd);
 
 	f2fs_balance_fs(sbi, true);
-=======
-	d_instantiate_new(dentry, inode);
->>>>>>> b90b641... do d_instantiate/unlock_new_inode combinations safely
 	return err;
 out:
 	handle_failed_inode(inode);
@@ -559,7 +555,8 @@ static int f2fs_mkdir(struct inode *dir, struct dentry *dentry, umode_t mode)
 
 	alloc_nid_done(sbi, inode->i_ino);
 
-	d_instantiate_new(dentry, inode);
+	d_instantiate(dentry, inode);
+	unlock_new_inode(inode);
 
 	if (IS_DIRSYNC(dir))
 		f2fs_sync_fs(sbi->sb, 1);
@@ -607,7 +604,6 @@ static int f2fs_mknod(struct inode *dir, struct dentry *dentry,
 	f2fs_unlock_op(sbi);
 
 	alloc_nid_done(sbi, inode->i_ino);
-<<<<<<< HEAD
 
 	d_instantiate(dentry, inode);
 	unlock_new_inode(inode);
@@ -616,9 +612,6 @@ static int f2fs_mknod(struct inode *dir, struct dentry *dentry,
 		f2fs_sync_fs(sbi->sb, 1);
 
 	f2fs_balance_fs(sbi, true);
-=======
-	d_instantiate_new(dentry, inode);
->>>>>>> b90b641... do d_instantiate/unlock_new_inode combinations safely
 	return 0;
 out:
 	handle_failed_inode(inode);
