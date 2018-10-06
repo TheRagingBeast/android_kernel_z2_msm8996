d63e3d7e4ddfc5770bb1937fd1a32835cf7b9481
The Hard Gamer V@RuN <gamelovr695@gmail.com>
2018-10-06 14:49:10 +0000

    Update fpc1020_ree.c

--- drivers/misc/fpc1020_ree.c
+++ drivers/misc/fpc1020_ree.c
@@ -403,7 +403,7 @@ static void set_fingerprintd_nice(int nice)
 
 	read_lock(&tasklist_lock);
 	for_each_process(p) {
-		if (!memcmp(p->comm, "fingerprintd", 16)) {
+		if (!memcmp(p->comm, "fingerprintd", 13)) {
 			pr_debug("fingerprint nice changed to %i\n", nice);
 			set_user_nice(p, nice);
 			break;
